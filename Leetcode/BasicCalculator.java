2c00979800af6b94589965bf97df709748bbf89f
ironspec07 <ironspec07@gmail.com>
2025-06-08 22:45:32 +0530

    Leetcode - 64

--- Leetcode/BasicCalculator.java
+++ Leetcode/BasicCalculator.java
@@ -1,8 +1,8 @@
-import java.util.*;
-public class BasicCalculator {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+// import java.util.*;
+// public class BasicCalculator {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String s = sc.nextLine();
         
-    }
-}
+//     }
+// }

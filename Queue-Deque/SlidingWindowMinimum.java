64ce2eca60dc42b8ace4ddc12c95cc7cff85f0e3
ironspec07 <ironspec07@gmail.com>
2025-07-02 00:48:39 +0530

    Leetcode-83 & Queue-Deque Complete

--- Queue-Deque/SlidingWindowMinimum.java
+++ Queue-Deque/SlidingWindowMinimum.java
@@ -1,10 +1,13 @@
 import java.util.*;
-public class SlidingWindowMaximum {
-    public static int[] maxSlidingWindow(int[] nums, int k) {
-        int[] ans = new int[nums.length-k+1];
+public class SlidingWindowMinimum {
+    public static int[] minSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return new int[0];
+        }
+        int[] ans = new int[nums.length - k + 1];
         Deque<Integer> dq = new ArrayDeque<>();
         for (int i = 0; i < k; i++) {
-            while (dq.size() > 0 && nums[i] > dq.getLast()) {
+            while (!dq.isEmpty() && nums[i] < dq.getLast()) {
                 dq.removeLast();
             }
             dq.addLast(nums[i]);
@@ -12,17 +15,18 @@ public class SlidingWindowMaximum {
         int idx = 0;
         ans[idx++] = dq.getFirst();
         for (int i = k; i < nums.length; i++) {
-            while (dq.size() > 0 && nums[i] > dq.getLast()) {
+            if (!dq.isEmpty() && dq.getFirst() == nums[i - k]) {
+                dq.removeFirst();
+            }
+            while (!dq.isEmpty() && nums[i] < dq.getLast()) {
                 dq.removeLast();
             }
             dq.addLast(nums[i]);
-            if (dq.getFirst() == nums[i-k]) {
-                dq.removeFirst();
-            }
             ans[idx++] = dq.getFirst();
         }
         return ans;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -31,9 +35,9 @@ public class SlidingWindowMaximum {
             arr[i] = sc.nextInt();
         }
         int k = sc.nextInt();
-        int[] ans = maxSlidingWindow(arr,k);
+        int[] ans = minSlidingWindow(arr, k);
         for (int i : ans) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
         sc.close();
     }

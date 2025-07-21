cb423821bd992fb6b6c60e4f1546ae1d26e4e638
ironspec07 <ironspec07@gmail.com>
2025-07-21 23:12:34 +0530

    Sub-60

--- Trees/RightView.java
+++ Trees/RightView.java
@@ -1,79 +1,79 @@
-package Trees;
-import java.util.*;
+// package Trees;
+// import java.util.*;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
+// class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode(int x) { val = x; }
+// }
 
-public class RightView {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+// public class RightView {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
         
-        TreeNode root = buildTree(arr);
-        List<Integer> leftView = leftView(root);
+//         TreeNode root = buildTree(arr);
+//         List<Integer> leftView = leftView(root);
         
-        for (int num : leftView) {
-            System.out.print(num + " ");
-        }
-        sc.close();
-    }
+//         for (int num : leftView) {
+//             System.out.print(num + " ");
+//         }
+//         sc.close();
+//     }
     
-    public static TreeNode buildTree(int[] arr) {
-        if (arr.length == 0 || arr[0] == -1) {
-            return null;
-        }
+//     public static TreeNode buildTree(int[] arr) {
+//         if (arr.length == 0 || arr[0] == -1) {
+//             return null;
+//         }
         
-        TreeNode root = new TreeNode(arr[0]);
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+//         TreeNode root = new TreeNode(arr[0]);
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
         
-        int i = 1;
-        while (!queue.isEmpty() && i < arr.length) {
-            TreeNode current = queue.poll();
+//         int i = 1;
+//         while (!queue.isEmpty() && i < arr.length) {
+//             TreeNode current = queue.poll();
             
-            if (i < arr.length && arr[i] != -1) {
-                current.left = new TreeNode(arr[i]);
-                queue.add(current.left);
-            }
-            i++;
+//             if (i < arr.length && arr[i] != -1) {
+//                 current.left = new TreeNode(arr[i]);
+//                 queue.add(current.left);
+//             }
+//             i++;
             
-            if (i < arr.length && arr[i] != -1) {
-                current.right = new TreeNode(arr[i]);
-                queue.add(current.right);
-            }
-            i++;
-        }
+//             if (i < arr.length && arr[i] != -1) {
+//                 current.right = new TreeNode(arr[i]);
+//                 queue.add(current.right);
+//             }
+//             i++;
+//         }
         
-        return root;
-    }
+//         return root;
+//     }
     
-    public static List<Integer> leftView(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (q.size()>0) {
-            int n = q.size();
-            for (int i = 1; i <= n; i++) {
-                TreeNode temp = q.remove();
-                if (i == n) {
-                    ans.add(temp.val);
-                }
-                if (temp.left != null) {
-                    q.add(temp.left);
-                }
-                if (temp.right != null) {
-                    q.add(temp.right);
-                }
-            }
-        }
-        return ans;
-    }
-}
+//     public static List<Integer> leftView(TreeNode root) {
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+//         while (q.size()>0) {
+//             int n = q.size();
+//             for (int i = 1; i <= n; i++) {
+//                 TreeNode temp = q.remove();
+//                 if (i == n) {
+//                     ans.add(temp.val);
+//                 }
+//                 if (temp.left != null) {
+//                     q.add(temp.left);
+//                 }
+//                 if (temp.right != null) {
+//                     q.add(temp.right);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
--- Trees/VerticalLevelTraversal.java
+++ Trees/VerticalLevelTraversal.java
@@ -1,115 +1,115 @@
-package Trees;
+// package Trees;
 
-import java.util.*;
+// import java.util.*;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+// class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
-    }
-}
+//     TreeNode(int x) {
+//         val = x;
+//     }
+// }
 
-class Pair {
-    TreeNode n;
-    int vLevel;
+// class Pair {
+//     TreeNode n;
+//     int vLevel;
 
-    Pair(TreeNode n1, int x) {
-        n = n1;
-        vLevel = x;
-    }
-}
+//     Pair(TreeNode n1, int x) {
+//         n = n1;
+//         vLevel = x;
+//     }
+// }
 
-public class VerticalLevelTraversal {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+// public class VerticalLevelTraversal {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
 
-        TreeNode root = buildTree(arr);
-        List<List<Integer>> VerticalLevel = VerticalLevel(root);
+//         TreeNode root = buildTree(arr);
+//         List<List<Integer>> VerticalLevel = VerticalLevel(root);
 
-        for (List<Integer> num : VerticalLevel) {
-            for (int num1 : num) {
-                System.out.print(num1+" ");
-            }
-            System.out.println();
-        }
-        sc.close();
-    }
+//         for (List<Integer> num : VerticalLevel) {
+//             for (int num1 : num) {
+//                 System.out.print(num1+" ");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
 
-    public static TreeNode buildTree(int[] arr) {
-        if (arr.length == 0 || arr[0] == -1) {
-            return null;
-        }
+//     public static TreeNode buildTree(int[] arr) {
+//         if (arr.length == 0 || arr[0] == -1) {
+//             return null;
+//         }
 
-        TreeNode root = new TreeNode(arr[0]);
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+//         TreeNode root = new TreeNode(arr[0]);
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
 
-        int i = 1;
-        while (!queue.isEmpty() && i < arr.length) {
-            TreeNode current = queue.poll();
+//         int i = 1;
+//         while (!queue.isEmpty() && i < arr.length) {
+//             TreeNode current = queue.poll();
 
-            if (i < arr.length && arr[i] != -1) {
-                current.left = new TreeNode(arr[i]);
-                queue.add(current.left);
-            }
-            i++;
+//             if (i < arr.length && arr[i] != -1) {
+//                 current.left = new TreeNode(arr[i]);
+//                 queue.add(current.left);
+//             }
+//             i++;
 
-            if (i < arr.length && arr[i] != -1) {
-                current.right = new TreeNode(arr[i]);
-                queue.add(current.right);
-            }
-            i++;
-        }
+//             if (i < arr.length && arr[i] != -1) {
+//                 current.right = new TreeNode(arr[i]);
+//                 queue.add(current.right);
+//             }
+//             i++;
+//         }
 
-        return root;
-    }
+//         return root;
+//     }
 
-    public static List<List<Integer>> VerticalLevel(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
-        q.add(new Pair(root, 0));
+//     public static List<List<Integer>> VerticalLevel(TreeNode root) {
+//         Queue<Pair> q = new LinkedList<>();
+//         HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+//         q.add(new Pair(root, 0));
 
-        while (q.size() > 0) {
-            Pair rem = q.remove();
-            TreeNode remN = rem.n;
-            int remVL = rem.vLevel;
+//         while (q.size() > 0) {
+//             Pair rem = q.remove();
+//             TreeNode remN = rem.n;
+//             int remVL = rem.vLevel;
 
-            if (!hm.containsKey(remVL)) {
-                hm.put(remVL, new ArrayList<Integer>());
-            }
-            hm.get(remVL).add(remN.val);
+//             if (!hm.containsKey(remVL)) {
+//                 hm.put(remVL, new ArrayList<Integer>());
+//             }
+//             hm.get(remVL).add(remN.val);
 
-            if (remN.left != null) {
-                q.add(new Pair(remN.left, remVL - 1));
-            }
-            if (remN.right != null) {
-                q.add(new Pair(remN.right, remVL + 1));
-            }
-        }
+//             if (remN.left != null) {
+//                 q.add(new Pair(remN.left, remVL - 1));
+//             }
+//             if (remN.right != null) {
+//                 q.add(new Pair(remN.right, remVL + 1));
+//             }
+//         }
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (Integer key : hm.keySet()) {
-            if (key < min) {
-                min = key;
-            }
-            if (key > max) {
-                max = key;
-            }
-        }
+//         int min = Integer.MAX_VALUE;
+//         int max = Integer.MIN_VALUE;
+//         for (Integer key : hm.keySet()) {
+//             if (key < min) {
+//                 min = key;
+//             }
+//             if (key > max) {
+//                 max = key;
+//             }
+//         }
 
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = min; i <= max; i++) {
-            ans.add(hm.get(i));
-        }
-        return ans;
-    }
-}
+//         List<List<Integer>> ans = new ArrayList<>();
+//         for (int i = min; i <= max; i++) {
+//             ans.add(hm.get(i));
+//         }
+//         return ans;
+//     }
+// }

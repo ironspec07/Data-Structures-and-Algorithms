8720c8ba70ac949b489323b636a8eee024ee674f
ironspec07 <ironspec07@gmail.com>
2025-08-05 23:28:18 +0530

    Sub-66

--- Graphs/Dijkstra.java
+++ Graphs/Dijkstra.java
@@ -1,12 +1,12 @@
-package Graphs;
-import java.util.*;
-public class Dijkstra {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int src = sc.nextInt();
+// package Graphs;
+// import java.util.*;
+// public class Dijkstra {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int m = sc.nextInt();
+//         int src = sc.nextInt();
         
         
-    }
-}
+//     }
+// }
--- Trees/SearchInBST.java
+++ Trees/SearchInBST.java
@@ -1,9 +1,9 @@
-//Leetcode-700
-import java.util.*;
-public class SearchInBST {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[]
-    }
-}
+// //Leetcode-700
+// import java.util.*;
+// public class SearchInBST {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+        
+//     }
+// }

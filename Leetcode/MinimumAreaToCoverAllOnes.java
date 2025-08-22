9d24a5a35de108e333a78c940bf0847812bb5966
ironspec07 <ironspec07@gmail.com>
2025-08-23 01:25:27 +0530

    Leetcode-109

---
+++ Leetcode/MinimumAreaToCoverAllOnes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class MinimumAreaToCoverAllOnes {
+    public static int minimumArea(int[][] grid) {
+        int minRow = grid.length, maxRow = -1;
+        int minCol = grid[0].length, maxCol = -1;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
+                }
+            }
+        }
+
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int ans = minimumArea(arr);
+        System.out.println(ans);
+        sc.close();
+    }
+}
